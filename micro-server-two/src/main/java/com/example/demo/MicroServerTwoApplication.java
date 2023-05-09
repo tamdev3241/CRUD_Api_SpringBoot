4a3dea94fdc9c20e5a2de628a207c1d63121a2ca
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-05-09 10:20:34 +0700

    initial project

---
+++ micro-server-two/src/main/java/com/example/demo/MicroServerTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServerTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServerTwoApplication.class, args);
+	}
+
+}
